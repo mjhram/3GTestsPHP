230c1c44fb02ac575228b14f513558fd8f2e307f
Mohammad J. Haider <mjhram2000@gmail.com>
2016-07-20 15:14:08 +0300

    initial

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+session_name('aTTS');
+session_start();
+?>
+
 <head>
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <title>Internet Test</title>
