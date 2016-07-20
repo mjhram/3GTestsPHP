8052d836e05ab8d1c60ad9338a4bb07f4802b22d
Mohammad J. Haider <mjhram2000@gmail.com>
2016-07-20 15:16:30 +0300

    Bug Fix

--- index.php
+++ index.php
@@ -16,9 +16,6 @@ session_start();
 <body>
 <?php
  
-error_reporting(E_ALL ^ E_NOTICE);
-session_name('aTTS');
-session_start();
 if($_SESSION['ar_en']==1) {
 	header('location: en/index.php');
 } else {
