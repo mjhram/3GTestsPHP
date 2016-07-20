8997da3e7648dbc5daa98614fdc8e77ec69e3271
Mohammad J. Haider <mjhram2000@gmail.com>
2016-07-20 15:59:47 +0300

    Bug Fix

--- t.php
+++ t.php
@@ -6,7 +6,13 @@
  * Time: 3:48 PM
  */
 
+
 error_reporting(E_ALL ^ E_NOTICE);
 session_name('aTTS');
 session_start();
+if($_SESSION['ar_en']==1) {
+    header('location: en/index.php');
+} else {
+    header('location: en/index.php');
+}
 ?>
