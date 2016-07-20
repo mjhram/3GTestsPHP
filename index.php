bc358e4f39b28d30b9452fbb19b1b155ff32dd57
Mohammad J. Haider <mjhram2000@gmail.com>
2016-07-20 16:00:39 +0300

    Bug Fix

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Internet Test</title>
-</head>
-
-<body>
 <?php
- 
+
 error_reporting(E_ALL ^ E_NOTICE);
 session_name('aTTS');
 session_start();
@@ -18,6 +9,3 @@ if($_SESSION['ar_en']==1) {
 	header('location: en/index.php');
 }
 ?>
-</body>
-
-</html>
