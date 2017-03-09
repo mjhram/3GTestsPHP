e58b38fd3558770ca40ca47d626cb672172378cc
Mohammad J. Haider <mjhram2000@gmail.com>
2017-03-10 02:21:57 +0300

    add wifi SSID

--- addTest.php
+++ addTest.php
@@ -6,13 +6,13 @@ require 'connect.php';
 session_name('aTTS');
 session_start();
 $a = mysqli_query($GLOBALS["___mysqli_ston"], "START TRANSACTION");
-$b="INSERT INTO 3gTests(`deviceId`,`imsi`, `phoneNumber`, `imei`,`netOperator`, `netName`, `netType`, `netClass`,`phoneType`,`mobileState`, `cid`, `cid_3g`, `rnc`, `lac`, `rssi`,".
+$b="INSERT INTO 3gTests(`deviceId`,`imsi`, `phoneNumber`, `imei`,`netOperator`, `netName`, `netType`, `netClass`,`phoneType`,`mobileState`, `wifissid`, `cid`, `cid_3g`, `rnc`, `lac`, `rssi`,".
 		" `lon`, `lat`, `minTxRate`, `maxTxRate`, `avTxRate`, `minRxRate`, `maxRxRate`, `avRxRate`, `Brand`, `Manufacturer`, `Model`, `Product`".
 		",`deviceId2`,`imsi2`, `phoneNum2`,`netOperator2`, `netName2`, `netType2`, `netClass2`, `SignalStrengths`".
 		",`nei`,`tmp`) ".
      		//"VALUES('imsi', 'phoneNumber', 'imei','netOperator', 'netName', '1',       'netClass','1',         '1',                  '1', '1', '1', '1', '1')";
      		" VALUES('{$_POST['deviceId']}', '{$_POST['imsi']}', '{$_POST['phoneNumber']}', '{$_POST['imei']}', '{$_POST['netOperator']}', '{$_POST['netName']}',".
-     		" '{$_POST['netType']}', '{$_POST['netClass']}', '{$_POST['phoneType']}', '{$_POST['mobileState']}', '{$_POST['cid']}', '{$_POST['cid_3g']}', '{$_POST['rnc']}', '{$_POST['lac']}', '{$_POST['rssi']}',".
+     		" '{$_POST['netType']}', '{$_POST['netClass']}', '{$_POST['phoneType']}', '{$_POST['mobileState']}', '{$_POST['wifissid']}', '{$_POST['cid']}', '{$_POST['cid_3g']}', '{$_POST['rnc']}', '{$_POST['lac']}', '{$_POST['rssi']}',".
      		" '{$_POST['lon']}', '{$_POST['lat']}', '{$_POST['minTxRate']}', '{$_POST['maxTxRate']}', '{$_POST['avTxRate']}', '{$_POST['minRxRate']}', '{$_POST['maxRxRate']}', '{$_POST['avRxRate']}',  '{$_POST['brand']}', '{$_POST['manuf']}', '{$_POST['model']}', '{$_POST['product']}',".
      		" '{$_POST['deviceId2']}', '{$_POST['imsi2']}', '{$_POST['phoneNum2']}', '{$_POST['netOperator2']}', '{$_POST['netName2']}', '{$_POST['netType2']}', '{$_POST['netClass2']}', '{$_POST['SignalStrengths']}'".
      		", '{$_POST['nei']}', '{$_POST['tmp']}')";
