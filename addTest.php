bc30f80eab9e59804be9fd563af7a24ac3ed466e
Mohammad J. Haider <mjhram2000@gmail.com>
2017-03-11 22:17:18 +0300

    log $_POST

--- addTest.php
+++ addTest.php
@@ -35,7 +35,7 @@ if($a) {
 	echo $b;
 	$res = "failed";
 }
-{ 
+{
 	$aSql = "INSERT INTO log(user, details, result, ip,url,sitename, params) VALUES(";
 	if(!$_SESSION['usr']) {
 		$aSql .= "-1,";
@@ -43,6 +43,8 @@ if($a) {
 		$aSql .= "{$_SESSION['id']},";
 	}
 	$pst = var_export($_POST, true);
+	$pst = mysqli_real_escape_string($GLOBALS["___mysqli_ston"], $pst);
+
 	$aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."', '{$pst}')";
 	mysqli_query($GLOBALS["___mysqli_ston"], $aSql);
 }
