c9123670a1a9ff7463f613ad984fa096b213384f
Mohammad J. Haider <mjhram2000@gmail.com>
2017-03-11 21:27:36 +0300

    log $_POST

--- addTest.php
+++ addTest.php
@@ -35,14 +35,14 @@ if($a) {
 	echo $b;
 	$res = "failed";
 }
-{
+{ 
 	$aSql = "INSERT INTO log(user, details, result, ip,url,sitename, params) VALUES(";
 	if(!$_SESSION['usr']) {
 		$aSql .= "-1,";
 	} else {
 		$aSql .= "{$_SESSION['id']},";
 	}
-	$pst = var_export($_POST, true);  
+	$pst = var_export($_POST, true);
 	$aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."', '{$pst}')";
 	mysqli_query($GLOBALS["___mysqli_ston"], $aSql);
 }
--- test.php
+++ test.php
@@ -1,16 +1,18 @@
 <?php
     require 'connect.php';
 
+define ("site_name",'test.php');
+$res = "failed";
+
 {
-    define ("site_name",'test.php');
-    $res = "failed";
-    $aSql = "INSERT INTO log(user, details, result, ip,url,sitename) VALUES(";
+    $aSql = "INSERT INTO log(user, details, result, ip,url,sitename, params) VALUES(";
     if(!$_SESSION['usr']) {
         $aSql .= "-1,";
     } else {
         $aSql .= "{$_SESSION['id']},";
     }
-    $aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."')";
+    $pst = var_export($_POST, true);
+    $aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."', '{$pst}')";
     mysqli_query($GLOBALS["___mysqli_ston"], $aSql);
     echo $aSql;
 }
