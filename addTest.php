516d92b9f333750de5bdb3eb9fe7e9950243e413
Mohammad J. Haider <mjhram2000@gmail.com>
2017-03-11 20:41:38 +0300

    log addTest

--- addTest.php
+++ addTest.php
@@ -31,13 +31,14 @@ if($a) {
 	$res = "failed";
 }
 {
-	$aSql = "INSERT INTO log(user, details, result, ip,url,sitename) VALUES(";
+	$aSql = "INSERT INTO log(user, details, result, ip,url,sitename,params) VALUES(";
 	if(!$_SESSION['usr']) {
 		$aSql .= "-1,";
 	} else {
 		$aSql .= "{$_SESSION['id']},";
 	}
-	$aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."')";
+	$pst = implode(",",$_POST);
+	$aSql .= "'addTest', '{$res}', '{$_SERVER['REMOTE_ADDR']}','{$_SERVER['REQUEST_URI']}','".site_name."', '{$pst}')";
 	mysqli_query($GLOBALS["___mysqli_ston"], $aSql);
 }
 ?>
